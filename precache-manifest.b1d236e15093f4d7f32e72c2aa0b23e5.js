28f9560648bf27893ec77c90b3111f695c14ed52
Citlalli DMG <citlallidmg@gmail.com>
2018-10-22 19:39:19 -0500

    Updates

---
+++ precache-manifest.b1d236e15093f4d7f32e72c2aa0b23e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fcee94e9f2b30e89353d",
+    "url": "/burger-queen-react/static/js/runtime~main.fcee94e9.js"
+  },
+  {
+    "revision": "5c9cc4dbc893099bdc9f",
+    "url": "/burger-queen-react/static/js/main.5c9cc4db.chunk.js"
+  },
+  {
+    "revision": "cf7cb7dc1373b0810215",
+    "url": "/burger-queen-react/static/js/1.cf7cb7dc.chunk.js"
+  },
+  {
+    "revision": "5c9cc4dbc893099bdc9f",
+    "url": "/burger-queen-react/static/css/main.e138650f.chunk.css"
+  },
+  {
+    "revision": "cf7cb7dc1373b0810215",
+    "url": "/burger-queen-react/static/css/1.86c70862.chunk.css"
+  },
+  {
+    "revision": "edfaeae1ef106c684abc6dfc97a28510",
+    "url": "/burger-queen-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/burger-queen/precache-manifest.6eb13d602ebd8a162cdfabc66e27b167.js"
+  "/burger-queen-react/precache-manifest.b1d236e15093f4d7f32e72c2aa0b23e5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/burger-queen/index.html", {
+workbox.routing.registerNavigationRoute("/burger-queen-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
