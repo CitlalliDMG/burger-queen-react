5e1ee51ce9d48c76742fa430576fac6391b852c4
Citlalli DMG <citlallidmg@gmail.com>
2018-10-25 23:46:49 -0500

    Updates

--- precache-manifest.d1a5f819cd3660327ca9663101624293.js
+++ precache-manifest.d1a5f819cd3660327ca9663101624293.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/burger-queen-react/static/js/runtime~main.fcee94e9.js"
   },
   {
-    "revision": "2bbee1ac9ccbe04b892a",
-    "url": "/burger-queen-react/static/js/main.2bbee1ac.chunk.js"
+    "revision": "3165741a8f1894498498",
+    "url": "/burger-queen-react/static/js/main.3165741a.chunk.js"
   },
   {
     "revision": "2a2933c2136c077cdbbd",
     "url": "/burger-queen-react/static/js/1.2a2933c2.chunk.js"
   },
   {
-    "revision": "2bbee1ac9ccbe04b892a",
+    "revision": "3165741a8f1894498498",
     "url": "/burger-queen-react/static/css/main.41ebf49a.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/burger-queen-react/static/css/1.908210c1.chunk.css"
   },
   {
-    "revision": "22ca8b648c197d0fb8e9cd9d27010aba",
+    "revision": "f9e01da63975c59e4f9096be948bff9c",
     "url": "/burger-queen-react/index.html"
   }
 ];
